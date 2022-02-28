ab619a9e7e1655bf48eb4e0e54f8bcdf237d7d46
Eutene Lin <eutenelin@gmail.com>
2022-02-28 22:42:52 +0800

    update schedual routers

--- routers/calenderNote.js
+++ routers/calenderNote.js
@@ -3,15 +3,15 @@ const router = express.Router()
 const connection = require('../utils/db')
 const moment = require('moment')
 
-// 行事曆的日曆事件API
+// 行事曆記事事件API
 router.get('/:year/:month', async (req, res, next) => {
   // http://localhost:3002/api/calendarE/:year/:month
   let [data, fields] = await connection.execute(
-    'SELECT  day(date) AS date, GROUP_CONCAT(DISTINCT category_id) AS category_id FROM Schedules WHERE year(date) = ? AND month(date)= ?  group by DATE(date) order by DATE(date);',
+    'SELECT pet_id AS pet_id, DAY(DATE) AS DATE, importance AS importance, tags AS tags, category_id AS category_id, title AS title, status AS status FROM Schedules WHERE year(date) = ? AND month(date)= ? ORDER BY DATE ASC;',
     [req.params.year, req.params.month]
   )
   data.map((d) => {
-    d.category_id = d.category_id.split(',')
+    d.tags = d.tags.split(',')
   })
   res.json(data)
 })
