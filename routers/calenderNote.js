903abfc14be4e56c81481f9c8e4230ae55c4172a
Chris <tanghsuwei@gmail.com>
2022-03-01 17:45:57 +0800

    資料表大小寫修正

--- routers/calenderNote.js
+++ routers/calenderNote.js
@@ -7,7 +7,7 @@ const moment = require('moment')
 router.get('/:year/:month', async (req, res, next) => {
   // http://localhost:3002/api/calendarE/:year/:month
   let [data, fields] = await connection.execute(
-    'SELECT pet_id AS pet_id, DAY(DATE) AS DATE, importance AS importance, tags AS tags, category_id AS category_id, title AS title, status AS status FROM Schedules WHERE year(date) = ? AND month(date)= ? ORDER BY DATE ASC;',
+    'SELECT pet_id AS pet_id, DAY(DATE) AS DATE, importance AS importance, tags AS tags, category_id AS category_id, title AS title, status AS status FROM schedules WHERE year(date) = ? AND month(date)= ? ORDER BY DATE ASC;',
     [req.params.year, req.params.month]
   )
   data.map((d) => {
