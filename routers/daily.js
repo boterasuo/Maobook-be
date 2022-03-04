b028ea2bf398f84abe8d7f27ab3268a90fe7dfdb
Ben-Buli <84383897+Ben-Buli@users.noreply.github.com>
2022-03-04 12:40:31 +0800

    社群的 router

--- routers/daily.js
+++ routers/daily.js
@@ -13,8 +13,6 @@ const moment = require("moment");
 //       res.json(data);
 //     });
 
-
-
 // 全部資料
 router.get("/card-list", async (req, res, next) => {
   let [data, field] = await connection.execute(
@@ -53,44 +51,48 @@ SELECT
   res.json(data);
 });
 
-// 對應的按讚列表 (貼文 x 按讚內容) 
+// 對應的按讚列表 (貼文 x 按讚內容)
 router.get("/like-list/:likeDiaryID", async (req, res) => {
   let [data, fields] = await connection.execute(
-`
+    `
 SELECT users.name, GROUP
 FROM diary_like AS likes
 JOIN users on likes.user_id = users.id 
-WHERE likes.diary_id = ?`,[req.params.likeDiaryID]);
+WHERE likes.diary_id = ?`,
+    [req.params.likeDiaryID]
+  );
   res.json(data);
 });
 
-// 對應的留言列表 (貼文 x 留言內容) 
-  router.get("/comment-list/:diaryId", async (req, res) => {
-    let [data, fields] = await connection.execute(
-  ` SELECT comments.id, comments.diary_id, users.name, users.image AS avatar, comments.comment, comments.created_at
+// 對應的留言列表 (貼文 x 留言內容)
+router.get("/comment-list/:diaryId", async (req, res) => {
+  let [data, fields] = await connection.execute(
+    ` SELECT comments.id, comments.diary_id, users.name, users.image AS avatar, comments.comment, comments.created_at
   FROM diary_comments AS comments
   JOIN users on comments.user_id = users.id
-  WHERE comments.diary_id = ?`,[req.params.diaryId])
-    res.json(data);
-  });
+  WHERE comments.diary_id = ?`,
+    [req.params.diaryId]
+  );
+  res.json(data);
+});
 
-  // router.get("/comment-list/:diaryId", async (req, res) => {
-  //   let [data, fields] = await connection.execute(
-  // ` SELECT comments.id, comments.diary_id, users.name, users.image AS commenter, comments.comment, comments.created_at
-  // FROM diary_comments AS comments
-  // JOIN users on comments.user_id = users.id
-  // WHERE comments.diary_id = ?
-  // ORDER BY created_at DESC `,[req.params.diaryId])
-  //   res.json(data);
-  // });
+// router.get("/comment-list/:diaryId", async (req, res) => {
+//   let [data, fields] = await connection.execute(
+// ` SELECT comments.id, comments.diary_id, users.name, users.image AS commenter, comments.comment, comments.created_at
+// FROM diary_comments AS comments
+// JOIN users on comments.user_id = users.id
+// WHERE comments.diary_id = ?
+// ORDER BY created_at DESC `,[req.params.diaryId])
+//   res.json(data);
+// });
 
-  // 送出貼文留言API
-  router.post('/AddComment', async (req, res, next) => {
+// 送出貼文留言API
+router.post("/AddComment", async (req, res, next) => {
   //  req.session.member.id取得登入會員id
-    let [data, fields] = await connection.execute(
-      `INSERT INTO diary_comments (id, diary_id, user_id, comment, created_at	) VALUES (?,?,?,?,?)`,[req.params.diary_id],[req.session.member.id],[req.params.comment],[req.params.date]);
-      res.json(data);
-   
-    });
+  let [data, fields] = await connection.execute(
+    `INSERT INTO diary_comments (id, diary_id, user_id, comment, created_at	) VALUES ('${req.body.id}','${req.body.cardID}','${req.session.member.id}','${req.body.comment}',NOW())`
+ );
+  res.json(data);
+});
 
 module.exports = router;
--- routers/discuss.js
+++ routers/discuss.js
@@ -1,32 +1,82 @@
-const express = require ("express");
+const express = require("express");
 const router = express.Router();
 const connection = require("../utils/db");
 const { checkLogin } = require("../middlewares/auth");
 const path = require("path");
 const moment = require("moment");
 
-   
- // 討論文列表
-router.get("/dicuss-list", async (req, res, next) => {
-    let [data, field] = await connection.execute(
-      `
-      SELECT 
-  	discussion.*,
-    COUNT(discussion_like.user_id) AS likes, 
-    COUNT(discussion_comments.user_id) AS comments,
-    discussion_category.name AS Category
-    
-    FROM social_discussion AS discussion
-    
-    JOIN discussion_comments ON discussion_comments.id = discussion_comments.discussion_id
-    JOIN discussion_like ON discussion.id = discussion_like.discussion_id
-    JOIN discussion_category ON discussion.category_id = discussion_category.id
-    
-    GROUP BY discussion_comments.discussion_id;
+// 討論文列表
+router.get("/bar-list", async (req, res, next) => {
+  let [data, field] = await connection.execute(
     `
-    );
-    res.json(data);
-  });
+      SELECT 
+      secondCombine.*, 
+      Month(secondCombine.created_at) AS month,
+      Day(secondCombine.created_at) AS date,
+      users.image AS avatar,
+      users.name As poster,
+      discussion_category.name AS category
+      FROM
+    (
+    SELECT 
+      combiner.*,
+    COUNT(discussion_like.user_id) AS likes
+      FROM
+    (
+    SELECT 
+    discuss.*,
+    COUNT(discussion_comment.user_id) AS discussion
+    FROM social_discussion AS discuss
+    JOIN discussion_comment ON discussion_comment.discussion_id = discuss.id
+    GROUP BY discussion_comment.discussion_id
+    ) AS combiner
+ 
+     LEFT JOIN discussion_like ON discussion_like.discussion_id = combiner.id
+     GROUP BY discussion_like.discussion_id
+    ) AS secondCombine
+    
+    LEFT JOIN discussion_category ON discussion_category.id = secondCombine.category_id
+    LEFT JOIN users ON users.id = secondCombine.user_id
+       
+    `
+  );
+  res.json(data);
+});
 
-  
+// 對應的按讚列表 (貼文 x 按讚內容)
+router.get("/like-list/:likediscussionID", async (req, res) => {
+  let [data, fields] = await connection.execute(
+    `
+SELECT users.name, GROUP
+FROM discussion_like AS likes
+JOIN users on likes.user_id = users.id 
+WHERE likes.discussion_id = ?`,
+    [req.params.likediscussionID]
+  );
+  res.json(data);
+});
+
+// 對應的留言列表 (貼文 x 留言內容)
+router.get("/comment-list/:barId", async (req, res) => {
+  let [data, fields] = await connection.execute(
+    ` SELECT discussion.id, discussion.discussion_id, users.name, users.image AS avatar, discussion.comment, discussion.created_at
+  FROM discussion_comment AS discussion
+  JOIN users on discussion.user_id = users.id
+  WHERE discussion.discussion_id = ?`,
+    [req.params.barId]
+  );
+  res.json(data);
+});
+
+// 送出貼文留言API
+router.post("/AddComment", async (req, res, next) => {
+  //  req.session.member.id取得登入會員id
+  let [data, fields] = await connection.execute(
+    `INSERT INTO discussion_comment (id, discussion_id, user_id, comment, created_at	) VALUES ('${req.body.id}','${req.body.barID}','${req.session.member.id}','${req.body.comment}',NOW())`
+ );
+  res.json(data);
+});
+
+
+
 module.exports = router;
