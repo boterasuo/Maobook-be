4532717ed89543eae007dec9d468ae748a47471e
Eutene Lin <eutenelin@gmail.com>
2022-03-06 16:11:46 +0800

    update nodemailer function

--- routers/nodemailer.js
+++ routers/nodemailer.js
@@ -10,6 +10,7 @@ const transporter = nodemailer.createTransport({
 });
 
 function sendEmail(email) {
+  
   const options = {
     from: process.env.MAIL_USER,
     to: email,
@@ -26,16 +27,41 @@ function sendEmail(email) {
   });
 }
 
-function sendCaseMail(email) {
+function sendCaseMail(email,eventType) {
+
+  // 判斷事件種類
+  let subjectstring='';
+  let htmlstring='';
+  
+    switch (eventType) {
+      case 1:
+        subjectstring="Maobook 毛毛日記：關於您應徵的互助，毛主人婉拒了您幫忙。"
+        htmlstring="<h2>感謝您熱心的參與毛孩互助</h2><p>目前對方毛主人因其他考量，已婉拒您的幫忙</p><p>但仍舊非常感謝您的參與，期待下次互助的機會</p>"
+        break;
+      case 2:
+        subjectstring="Maobook 毛毛日記：關於您應徵的互助，毛主人想與您進一步聯繫。"
+        htmlstring="<h2>感謝您熱心的參與毛孩互助</h2><p>對方毛主人想進一步與您媒合與溝通詳細資訊</p><p>請您留意最近的電子信箱與手機，以防錯過訊息喔！</p>"
+        break;        
+      case 3:
+        subjectstring="Maobook 毛毛日記：關於您應徵的互助，您已成功接案此委託。"
+        htmlstring="<h2>感謝您熱心的參與毛孩互助</h2><p>您已完成媒合，成功接受此互助案件</p><p>請記得於約定的時間地點，完成委託喔！</p>"
+        break;
+      case 4:
+        subjectstring="Maobook 毛毛日記：關於您應徵的互助，此委託已結案，請確認後續金流。"
+        htmlstring="<h2>感謝您熱心的參與毛孩互助</h2><p>您所應徵的委託案已經順利完成，請確認是否收款</p><p>感謝您的參與，期待下次互助的機會</p>"
+        break;
+      default:
+        break;
+    }
+
   const options = {
     from: process.env.MAIL_USER,
     to: email,
-    subject: "Maobook 毛毛日記：你應徵的委託收到回覆囉！",
-    html: 
-    "<h2>你應徵的委託收到回覆囉</h2><p>點擊登入<a href='http://localhost:3000/login'><p>毛毛日記</p></a>看看對方回應您什麼吧～！</p>",
+    subject: subjectstring,
+    html: htmlstring,
   };
 
-  transporter.sendMail(options, function (err, info) {
+  transporter.sendCaseMail(options, function (err, info) {
     if (err) {
       console.log(err);
       return;
