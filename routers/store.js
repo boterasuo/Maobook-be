52e9b83e8733260f3c6690d2a20d563554ff0143
dummiss <dummiss1216@gmail.com>
2022-03-07 22:20:21 +0800

    更新

--- routers/store.js
+++ routers/store.js
@@ -31,44 +31,48 @@ router.get("/petlist", async (req, res, next) => { //從資料庫抓出會員ID�
 
 // "/api/store/recomProduct"
 router.get("/recomProduct", async (req, res, next) => { //從資料庫抓
-  petID = req.query.petID
-  //先抓出寵物的疾病ID
-  let [illnessID] = await connection.execute("SELECT illness_category_id FROM pet_illness WHERE pet_id=?", [petID]);
-  //轉成字串
-  let newillnessID = illnessID.map((d) => {
-    return (d.illness_category_id).toString()
-  })
-  //再從疾病產品中，找出包含這些疾病ID的產品
-  let [Rrecomproduct] = await connection.execute(`SELECT * FROM product_illness WHERE illness_category_id IN (${newillnessID})`);
-  // console.log("Rrecomproduct", Rrecomproduct)
+  if (req.query.petID) {
+    petID = req.query.petID
+    //先抓出寵物的疾病ID
+    let [illnessID] = await connection.execute("SELECT illness_category_id FROM pet_illness WHERE pet_id=?", [petID]);
+    //轉成字串
+    let newillnessID = illnessID.map((d) => {
+      return (d.illness_category_id).toString()
+    })
+    //再從疾病產品中，找出包含這些疾病ID的產品
+    let [Rrecomproduct] = await connection.execute(`SELECT * FROM product_illness WHERE illness_category_id IN (${newillnessID})`);
+    // console.log("Rrecomproduct", Rrecomproduct)
 
-  //再從商品中計算重複的次數，藉此找出在最符合的前三名
-  //1.先計算每個商品出現的次數
-  var result = [];
-  Rrecomproduct.reduce(function (res, value) {
-    if (!res[value.product_id]) {
-      res[value.product_id] = { product_id: value.product_id, count: 0 };
-      result.push(res[value.product_id])
-    }
-    res[value.product_id].count += 1;
-    return res;
-  }, {});
-  // console.log("count", result)
+    //再從商品中計算重複的次數，藉此找出在最符合的前三名
+    //1.先計算每個商品出現的次數
+    var result = [];
+    Rrecomproduct.reduce(function (res, value) {
+      if (!res[value.product_id]) {
+        res[value.product_id] = { product_id: value.product_id, count: 0 };
+        result.push(res[value.product_id])
+      }
+      res[value.product_id].count += 1;
+      return res;
+    }, {});
+    // console.log("count", result)
 
-  //2. 按照次數由大到小排序，藉此找出前三個最符合的商品
-  let newresult =
-    (result.sort(function (a, b) {
-      return a.count < b.count ? 1 : -1;
-    })).map((d) => { return (d.product_id).toString() })
-  // console.log("max3", newresult)
-
-
-  //3.進資料庫搜尋
-  let [finalresult] = await connection.execute(`SELECT * FROM products  WHERE id IN (${newresult})`)
-  console.log("finalresult", finalresult)
+    //2. 按照次數由大到小排序，藉此找出前三個最符合的商品
+    let newresult =
+      (result.sort(function (a, b) {
+        return a.count < b.count ? 1 : -1;
+      })).map((d) => { return (d.product_id).toString() })
+    // console.log("max3", newresult)
 
 
-  res.json(finalresult);
+    //3.進資料庫搜尋
+    let [finalresult] = await connection.execute(`SELECT * FROM products  WHERE id IN (${newresult})`)
+    console.log("finalresult", finalresult)
+
+    res.json(finalresult);
+  } else {
+
+    next();
+  }
 });
 
 
